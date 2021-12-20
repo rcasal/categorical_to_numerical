af51a3ddbc97a3df74d4e5aec6f0ffd407223e10
rcasal <38733103+rcasal@users.noreply.github.com>
2021-12-20 12:06:17 -0300

    Update utils.py

--- categorical_to_numerical/utils.py
+++ categorical_to_numerical/utils.py
@@ -1,8 +1,13 @@
-import pandas
-import numpy
+import numpy as np 
+import pandas as pd 
+
+from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.compose import make_column_selector as selector
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import balanced_accuracy_score, f1_score, accuracy_score, average_precision_score, roc_auc_score, cohen_kappa_score
+
 
 from .cat2num import cat2num
 
