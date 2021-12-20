f851040e67cc40ce0cf3e48ecbe6ae5ca2a8341f
rcasal <38733103+rcasal@users.noreply.github.com>
2021-12-20 12:47:58 -0300

    Update utils.py

--- categorical_to_numerical/utils.py
+++ categorical_to_numerical/utils.py
@@ -7,6 +7,7 @@ from sklearn.compose import make_column_selector as selector
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import balanced_accuracy_score, f1_score, accuracy_score, average_precision_score, roc_auc_score, cohen_kappa_score
+from sklearn.linear_model import LogisticRegression
 
 
 from .cat2num import cat2num
